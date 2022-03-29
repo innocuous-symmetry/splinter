b2ac098fdb4afba33413a273bf4e7d13e9c4c1b3
Mikayla Dobson <93477693+innocuous-symmetry@users.noreply.github.com>
2022-03-29 12:23:30 -0500

    establishing layout for mern stack

---
+++ client/src/components/Welcome.js
@@ -0,0 +1,13 @@
+export default function Welcome() {
+    return (
+        <>
+        <h1>Welcome to Splinter</h1>
+        <h2>Compete with your friends to have the lowest<br/>possible amount of resources to your name!</h2>
+
+        <h2>Choose from the options below:</h2>
+        <form className="game-config">
+            
+        </form>
+        </>
+    )
+}
